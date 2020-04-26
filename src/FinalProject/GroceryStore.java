e3254735411789cd24bb3e7401d7db37740a24fb
wang2021 <62254195+wang2021@users.noreply.github.com>
2020-04-26 12:34:50 +0800

    Update GroceryStore.java

--- src/FinalProject/GroceryStore.java
+++ src/FinalProject/GroceryStore.java
@@ -1,6 +1,8 @@
 package FinalProject;
 
+import java.util.Arrays;
 import java.util.Scanner;
+
 /**
  * 超市的parent class，会有各自的物品，价格，库存
  * 能够计算价格*数量
@@ -8,23 +10,22 @@ import java.util.Scanner;
  * 顾客购买后，库存会减少
  * 有补货功能，设为一天开始的原始数量
  * 参考grocery item order
- *
  */
 public class GroceryStore {
 
-    String[] itemName = {"A","B","C"};
+    String[] itemName = {"A", "B", "C"};
 
-    int[] itemPrice = {1,2,3};
+    int[] itemPrice = {1, 2, 3};
 
-    int[] itemQuantity = {20,20,20};
+    int[] itemQuantity = {20, 20, 20};
 
-    int[] defaultQuantity = {20,20,20};
+    int[] defaultQuantity = {20, 20, 20};
 
-    public GroceryStore(){
+    public GroceryStore() {
 
     }
 
-    public int receiveOrder(){
+    public int receiveOrder() {
 
         Scanner in = new Scanner(System.in);
         System.out.println("How many items you want to purchase today? ");
@@ -35,15 +36,17 @@ public class GroceryStore {
 
         System.out.println("Please enter an item to add to the list (item-name quantity price) "
                 + "Enter \"quit\" to stop adding items to the list");
-        while(! in.next().equals("quit")){
-            // itemOrderNames.add(console.next());
-            // itemOrderQuantity.add(console.nextInt());
+        while (!in.next().equals("quit")) {
+            for (int i = 0; i < size; i++) {
+                itemOrderNames[i] = in.next();
+                itemOrderQuantity[i] = in.nextInt();
+            }
         }
 
-        for(int i = 0; i < itemOrderNames.length; i++){
-            for(int j = 0; j < itemName.length; j++){
-                if(itemOrderNames[i].equals(itemName[j])){
-                    if((itemQuantity[j] - itemOrderQuantity[i] < 0 ){
+        for (int i = 0; i < itemOrderNames.length; i++) {
+            for (int j = 0; j < itemName.length; j++) {
+                if (itemOrderNames[i].equals(itemName[j])) {
+                    if ((itemQuantity[j] - itemOrderQuantity[i] < 0) {
                         throw new IllegalArgumentException("Item out of stock!");
                     }
                     itemQuantity[j] -= itemOrderQuantity[i];
@@ -53,27 +56,27 @@ public class GroceryStore {
 
     }
 
-    public int getQuantity(){
+    public int getQuantity() {
 
         return itemQuantity[receiveOrder()];
     }
 
-    public void setQuantity(){
+    public void setQuantity() {
 
-        return itemQuantity[receiveOrder()-];
-
-    }
-
-    public void restock(){
-
+        return itemQuantity[];
 
     }
 
-    public double getPrice(){
+    public void restock() {
+
 
     }
 
-    public String toString(){
+    public double getPrice() {
+
+    }
+
+    public String toString() {
 
     }
 }
